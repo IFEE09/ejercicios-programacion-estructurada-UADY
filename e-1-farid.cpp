98a85bce54f477a115560b1d7ef4c64ab6de7644
IFEE09 <ivanfaridespadas@gmail.com>
2022-06-09 00:47:38 -0500

    agregando primer ejercicio con su commit

---
+++ e-1-farid.cpp
@@ -0,0 +1,30 @@
+// Crear una funcion que sume dos numeros, otra que los reste
+// otra que los multiplique, y otra que los divida
+#include <stdio.h>
+
+// prototipos de funciones
+float suma(float num1, float num2);
+float resta(float num1, float num2);
+float multiplica(float num1, float num2);
+float divide(float num1, float num2);
+
+int main() {
+
+    float num1, num2;
+    float opcion;
+
+    printf("Ingrese el primer número: ");
+    scanf("%f", &num1);
+    printf("Ingrese el segundo número: ");
+    scanf("%f", &num2);
+
+    printf("Digíta 1 para sumar los números\n");
+    printf("Digíta 2 para restar los números\n");
+    printf("Digíta 3 para multiplicar los números\n");
+    printf("Digíta 4 para dividir los números\n");
+
+}
+
+float suma(int num1, int num2) {
+
+}
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,19 @@
+// Crear una funcion que sume dos numeros, otra que los reste
+// otra que los multiplique, y otra que los divida
+#include <stdio.h>
+
+// prototipos de funciones
+float suma(float num1, float num2);
+float resta(float num1, float num2);
+float multiplica(float num1, float num2);
+float divide(float num1, float num2);
+
+int main() {
+
+    float num1, num2;
+
+    printf("Ingrese el primer número: ");
+    scanf("%f", &num1);
+    
+    return 0;
+}
